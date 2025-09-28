1adb6792fb7135b3b98dc4c2a2d1a63fa8c3f622
danilo herrera hernandez <danilitohhh@gmail.com>
2025-09-28 15:03:50 -0500

    parte final de evidencia

--- tecnopunto/productos/urls.py
+++ tecnopunto/productos/urls.py
@@ -1,19 +1,11 @@
 from django.urls import path
+from . import views  # Importamos las vistas de la aplicación productos
 
-from .views import (
-    InicioView,
-    ProductoCreateView,
-    ProductoDeleteView,
-    ProductoListView,
-    ProductoUpdateView,
-)
 
 app_name = 'productos'
-
 urlpatterns = [
-    path('', ProductoListView.as_view(), name='list'),
-    path('nuevo/', ProductoCreateView.as_view(), name='create'),
-    path('<int:pk>/editar/', ProductoUpdateView.as_view(), name='update'),
-    path('<int:pk>/eliminar/', ProductoDeleteView.as_view(), name='delete'),
-    path('inicio/', InicioView.as_view(), name='inicio'),
+   path('', views.lista_productos, name='lista'),
+   path('nuevo/', views.create_product, name='nuevo'),
+   path('editar/<int:pk>/', views.edit_product, name='editar'),
+   path('eliminar/<int:pk>/', views.delete_product, name='eliminar'),
 ]
--- tecnopunto/tecnopunto/urls.py
+++ tecnopunto/tecnopunto/urls.py
@@ -1,26 +1,11 @@
-"""
-URL configuration for tecnopunto project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path,include
+from productos.views import inicio
 
-from productos.views import InicioView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', InicioView.as_view(), name='inicio'),
-    path('productos/', include('productos.urls')),
+   path('admin/', admin.site.urls),
+   path('', inicio, name='inicio'), #ruta para la página de inicio
+   path('productos/', include('productos.urls')),  # ruta para las vistas de productos
 ]
+
